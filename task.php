318bc4c4e4167016c7380fdcd82887f51f2a93b7
M Iftakher Ahammed <iahammed@gmail.com>
2018-07-08 00:37:11 +0100

    First commit

--- task.php
+++ task.php
@@ -3,19 +3,5 @@
 class task {
 	public $description;
 	public $completed;
-	// public function __construct($description = '', $completed = '')
-	// {
-	// 	$this->description = $description;
-	// 	$this->completed = $completed;
-	// }
-
-	// public function description()
-	// {
-	// 	return $this->decription;
-	// }
-	// public function completed()
-	// {
-	// 	$this->completed = true;
-	// }
 
 }
